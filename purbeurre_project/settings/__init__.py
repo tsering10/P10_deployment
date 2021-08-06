235ceee55dace082bb08c17cf3813c78146a1768
tsering10 <ttsering10@outlook.com>
2021-08-06 10:37:33 +0000

    changes in base_dir

--- purbeurre_project/settings/__init__.py
+++ purbeurre_project/settings/__init__.py
@@ -27,8 +27,8 @@ load_dotenv(dotenv_path=env_path)
 warnings.filterwarnings("ignore", message="No directory at", module="whitenoise.base" )
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = '/home/ttsering/P10_deployment/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = '/home/ttsering/P10_deployment/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
